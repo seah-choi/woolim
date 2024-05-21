aa767304bdba27ef94d2086932591fb7a5470faf
DESKTOP-7DN2GF6\pc <leehv1234@kookmin.ac.kr>
2024-05-21 20:34:22 +0900

    etc: 관리자 컨트롤러 연결

---
+++ src/main/java/org/fullstack4/woolim/controller/AdminClassController.java
@@ -0,0 +1,34 @@
+package org.fullstack4.woolim.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+@RequestMapping(value="/admin/class")
+@RequiredArgsConstructor
+public class AdminClassController {
+
+    @GetMapping("/list")
+    public void GETList() {
+
+    }
+
+    @GetMapping("/regist")
+    public void GETRegist() {
+
+    }
+
+    @GetMapping("/modify")
+    public void GETModify() {
+
+    }
+
+    @GetMapping("/delete")
+    public void GETDelete() {
+
+    }
+}
