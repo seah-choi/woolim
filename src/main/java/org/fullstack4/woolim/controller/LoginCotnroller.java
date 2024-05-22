4bcf07f930bdd278c6feac0e9b1efb570addbcc6
DESKTOP-7DN2GF6\pc <leehv1234@kookmin.ac.kr>
2024-05-22 12:30:04 +0900

    etc:test

--- src/main/java/org/fullstack4/woolim/controller/LoginCotnroller.java
+++ src/main/java/org/fullstack4/woolim/controller/LoginCotnroller.java
@@ -2,10 +2,14 @@ package org.fullstack4.woolim.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.fullstack4.woolim.dto.MemberDTO;
+import org.fullstack4.woolim.service.MemberServiceIf;
+import org.fullstack4.woolim.service.MemberServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Log4j2
 @Controller
@@ -13,14 +17,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class LoginCotnroller {
 
+    private final MemberServiceIf memberService;
     @GetMapping("/login")
     public void GETLogin() {
 
     }
 
     @PostMapping("/login")
-    public void POSTLogin() {
-
+    public void POSTLogin(MemberDTO memberDTO) {
+        log.info("memberDTO" + memberDTO);
+        boolean login = memberService.login(memberDTO);
+        if (login) {
+            log.info("로그인 성공");
+        }else{
+            log.info("로그인 실패");
+        }
+        
     }
 
     @GetMapping("/logout")
--- src/main/java/org/fullstack4/woolim/service/MemberServiceIf.java
+++ src/main/java/org/fullstack4/woolim/service/MemberServiceIf.java
@@ -1,4 +1,7 @@
 package org.fullstack4.woolim.service;
 
+import org.fullstack4.woolim.dto.MemberDTO;
+
 public interface MemberServiceIf {
+    public boolean login(MemberDTO memberDTO);
 }
