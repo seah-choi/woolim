ef342ec688239f57e31b9d9a6ce35e35298f0161
SungHoonJung777 <sshu777@naver.com>
2024-05-22 11:20:29 +0900

    -- 오류 수정

--- src/main/java/org/fullstack4/woolim/mapper/LectureMapper.java
+++ src/main/java/org/fullstack4/woolim/mapper/LectureMapper.java
@@ -7,13 +7,4 @@ import java.util.List;
 
 public interface LectureMapper {
     List<LectureVO> getList();
-
-<<<<<<< HEAD
-=======
-@Mapper
-public interface LectureMapper {
-<<<<<<< HEAD
->>>>>>> 9adfce3500614e623261e1c660456ea44e8db0bf
-=======
->>>>>>> 9adfce3500614e623261e1c660456ea44e8db0bf
 }
